e917f0b98b023e89c6842b323edccaeb206bff36
Christian Kilb <christian.kilb@gmail.com>
2023-07-22 17:50:20 +0200

    get rid of script, use single DB query

--- pocketbooksync.koplugin/main.lua
+++ pocketbooksync.koplugin/main.lua
@@ -2,6 +2,8 @@ local WidgetContainer = require("ui/widget/container/widgetcontainer")
 local _ = require("gettext")
 local logger = require("logger")
 local UIManager = require("ui/uimanager")
+local SQ3 = require("lua-ljsqlite3/init")
+local pocketbookDbConn = SQ3.open("/mnt/ext1/system/explorer-3/explorer-3.db")
 
 local PocketbookSync = WidgetContainer:extend{
     name = "pocketbooksync",
@@ -9,29 +11,57 @@ local PocketbookSync = WidgetContainer:extend{
 }
 
 function PocketbookSync:sync(title, page)
-    local command = "sh /mnt/ext1/applications/koreader/plugins/pocketbooksync.koplugin/sync.sh " .. page  .. " \"" .. title .. "\""
-    logger.info("Pocketbook Sync: Run sync: " .. command)
+    local totalPages = self.view.document:getPageCount()
+    if not totalPages then
+        logger.info("Pocketbook Sync: No total pages found " .. title .. ", " .. page)
+    end
 
-    os.execute(command)
+    local completed = 0
+
+    if page >= totalPages then
+        completed = 1
+    end
+
+    local stmt = [[
+            REPLACE INTO books_settings
+            (bookid, profileid, cpage, npage, completed, opentime)
+            VALUES
+            (
+                (SELECT id
+                FROM books_impl
+                WHERE TRIM(LOWER(title))=TRIM(LOWER(?))
+                ORDER BY id DESC
+                LIMIT 1),
+                1,
+                ?,
+                ?,
+                ?,
+                ?
+            )
+        ]]
+
+    stmt = pocketbookDbConn:prepare(stmt)
+
+    stmt:reset():bind(title, page, totalPages, completed, os.time(os.date("!*t"))):step()
 end
 
 function PocketbookSync:getTitle()
     local props = self.view.document:getProps()
 
-    logger.info("Pocketbook Sync: Get book title " .. props.title)
-
     return props.title
 end
 
 function PocketbookSync:onPageUpdate(page)
-    logger.info("Pocketbook Sync: Page update registered")
-
     UIManager:scheduleIn(3, function()
         local title = self:getTitle()
 
         if title ~= "" then
             self:sync(title, page);
+
+            return
         end
+
+        logger.info("Pocketbook Sync: Title not found")
     end)
 end
 
