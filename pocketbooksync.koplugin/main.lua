f5f7d3879a45d781a94e09fd327b24e66da10db2
Christian Kilb <christian.kilb@gmail.com>
2024-12-10 21:40:11 +0100

    improve performance by caching prepared data, remove scheduled syncing bc it slows down the process for some reason

--- pocketbooksync.koplugin/main.lua
+++ pocketbooksync.koplugin/main.lua
@@ -8,12 +8,12 @@ local WidgetContainer = require("ui/widget/container/widgetcontainer")
 local _ = require("gettext")
 local logger = require("logger")
 local util = require("util")
-local UIManager = require("ui/uimanager")
 local SQ3 = require("lua-ljsqlite3/init")
 local pocketbookDbConn = SQ3.open("/mnt/ext1/system/explorer-3/explorer-3.db")
 local ffi = require("ffi")
 local inkview = ffi.load("inkview")
 local bookIds = {}
+local preparedData = {}
 
 -- wait for database locks for up to 1 second before raising an error
 pocketbookDbConn:set_busy_timeout(1000)
@@ -37,14 +37,13 @@ local PocketbookSync = WidgetContainer:extend{
     is_doc_only = false,
 }
 
-function PocketbookSync:immediateSync()
-    UIManager:unschedule(self.doSync)
-    self:doSync(self:prepareSync())
+function PocketbookSync:clearCache()
+    bookIds = {}
+    preparedData = {}
 end
 
-function PocketbookSync:scheduleSync()
-    UIManager:unschedule(self.doSync)
-    UIManager:scheduleIn(3, self.doSync, self, self:prepareSync())
+function PocketbookSync:sync()
+    self:doSync(self:prepareSync())
 end
 
 function PocketbookSync:prepareSync()
@@ -55,47 +54,52 @@ function PocketbookSync:prepareSync()
         return nil
     end
 
-    local folder, file = self:getFolderFile()
-    if not folder or folder == "" or not file or file == "" then
-        logger.info("Pocketbook Sync: No folder/file found for " .. self.view.document.file)
-        return nil
+    local cacheKey = self.ui.document.file
+
+    if not preparedData[cacheKey] then
+        local folder, file = self:getFolderFile()
+        if not folder or folder == "" or not file or file == "" then
+            logger.info("Pocketbook Sync: No folder/file found for " .. self.view.document.file)
+            return nil
+        end
+
+        local globalPage = self.view.state.page
+        local flow = self.document:getPageFlow(globalPage)
+
+        -- skip sync if not in the main flow
+        if flow ~= 0 then
+            return nil
+        end
+
+        local totalPages = self.document:getTotalPagesInFlow(flow)
+        local page = self.document:getPageNumberInFlow(globalPage)
+
+        local summary = self.ui.doc_settings:readSetting("summary")
+        local status = summary and summary.status
+        local completed = (status == "complete" or page == totalPages) and 1 or 0
+
+        -- hide the progress bar if we're on the title/cover page
+        --
+        -- we'll never set cpage=1 so the progress bar will seem to jump a bit at
+        -- the start of a book, but there's no nice way to fix that: to use the
+        -- full range, we'd need to map pages 2 to last-1 to cpages 1 to last-1,
+        -- and that always skips one position; skipping the first one is the least
+        -- surprising behaviour
+        if page == 1 then
+            page = 0
+        end
+
+        preparedData[cacheKey] = {
+            folder = folder,
+            file = file,
+            totalPages = totalPages,
+            page = page,
+            completed = completed,
+            time = os.time(),
+        }
     end
-
-    local globalPage = self.view.state.page
-    local flow = self.document:getPageFlow(globalPage)
-
-    -- skip sync if not in the main flow
-    if flow ~= 0 then
-        return nil
-    end
-
-    local totalPages = self.document:getTotalPagesInFlow(flow)
-    local page = self.document:getPageNumberInFlow(globalPage)
-
-    local summary = self.ui.doc_settings:readSetting("summary")
-    local status = summary and summary.status
-    local completed = (status == "complete" or page == totalPages) and 1 or 0
-
-    -- hide the progress bar if we're on the title/cover page
-    --
-    -- we'll never set cpage=1 so the progress bar will seem to jump a bit at
-    -- the start of a book, but there's no nice way to fix that: to use the
-    -- full range, we'd need to map pages 2 to last-1 to cpages 1 to last-1,
-    -- and that always skips one position; skipping the first one is the least
-    -- surprising behaviour
-    if page == 1 then
-        page = 0
-    end
-
-    local data = {
-        folder = folder,
-        file = file,
-        totalPages = totalPages,
-        page = page,
-        completed = completed,
-        time = os.time(),
-    }
-    return data
+    
+    return preparedData[cacheKey]
 end
 
 function PocketbookSync:doSync(data)
@@ -147,19 +151,22 @@ function PocketbookSync:getFolderFile()
 end
 
 function PocketbookSync:onPageUpdate()
-    self:scheduleSync()
+    self:sync()
 end
 
 function PocketbookSync:onFlushSettings()
-    self:immediateSync()
+    self:clearCache()
+    self:sync()
 end
 
 function PocketbookSync:onCloseDocument()
-    self:immediateSync()
+    self:clearCache()
+    self:sync()
 end
 
 function PocketbookSync:onEndOfBook()
-    self:immediateSync()
+    self:clearCache()
+    self:sync()
 end
 
 return PocketbookSync
